6edafd59ea1b54004cf1ae1a3e69e5bfd618563c
Mitchell Oliver <Mitchelloliver1991@gmail.com>
2021-10-23 20:11:12 -0700

    searchbar styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,26 +1,39 @@
 import React, { useState } from "react";
+import "../styles/searchBar.css";
+import MagnifyGlass from "../assets/icon-search.svg";
 
 const SearchBar = ({ onSearch }) => {
   const [username, setUsername] = useState("");
 
   return (
-    <form>
-      <input
-        type="text"
-        placeholder="Search Github username..."
-        name="searchbox"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <button
-        onClick={(e) => {
-          onSearch(e, username);
-          setUsername("");
-        }}
-      >
-        Search
-      </button>
-    </form>
+    <div className="search-bar">
+      <form>
+        <div>
+          <img
+            className="magnify-glass"
+            src={MagnifyGlass}
+            alt="magnify-glass"
+          />
+          <input
+            className="search-box"
+            type="text"
+            placeholder="Search Github username..."
+            name="searchbox"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <button
+          className="search-button"
+          onClick={(e) => {
+            onSearch(e, username);
+            setUsername("");
+          }}
+        >
+          Search
+        </button>
+      </form>
+    </div>
   );
 };
 
