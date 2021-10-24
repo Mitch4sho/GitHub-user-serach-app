3dd804dbaa0ce855dd67602f8bfbaac2b699fe2e
Mitchell Oliver <Mitchelloliver1991@gmail.com>
2021-10-23 19:08:27 -0700

    styling for title

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import UserDashBoard from "./components/UserDashboard";
+import "./App.css";
+import Moon from "./assets/icon-moon.svg";
 
 function App() {
   const [currentUser, setCurrentUser] = useState({});
 
   const search = (e, username) => {
-    console.log("------searching------");
     e.preventDefault();
     fetch(`https://api.github.com/users/${username}`, {
       method: "GET",
     })
       .then((response) => response.json())
       .then((response) => {
-        // console.log(response);
         setCurrentUser(response);
       })
       .catch((err) => {
@@ -22,8 +22,14 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>devfinder</h1>
+    <div className="App">
+      <div className="title-container">
+        <h1 className="title">devfinder</h1>
+        <div className="dark-mode-button">
+          <p>Dark</p>
+          <img src={Moon} alt="moon-icon" />
+        </div>
+      </div>
       <SearchBar onSearch={search} />
       <UserDashBoard user={currentUser} />
     </div>
