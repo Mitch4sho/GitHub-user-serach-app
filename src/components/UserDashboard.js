6a759f0ee5df48e00cddd659379c821b7b72bae1
Mitchell Oliver <Mitchelloliver1991@gmail.com>
2021-11-24 19:06:39 -0800

    fix for user to have acces to github profile

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -19,7 +19,9 @@ const UserDashBoard = ({ user }) => {
           className="user-profile-pic"
         />
         <p className="name">{user.name}</p>
-        <p className="username">@{user.login}</p>
+        <p className="username">
+          <a href={user.html_url}>@{user.login}</a>
+        </p>
         <p className="join-date">{`Joined ${joinDate[2]} ${joinDate[1]} ${joinDate[0]}`}</p>
         <p className="profile-bio">{user.bio}</p>
       </header>
